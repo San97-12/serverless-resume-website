a40367f6626519be0df58e897782efc20fa8e2ec
Sanjeevini <sanjeevinip9@gmail.com>
2025-08-17 20:17:19 +0530

    update

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,44 +2,30 @@ import boto3
 import json
 from decimal import Decimal
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('pageviewresume')
+
 def lambda_handler(event, context):
     try:
-        dynamodb = boto3.resource('dynamodb', region_name='eu-east-1')
-        print("Connecting to table: pageviewresume in region eu-north-1")
-
-        table = dynamodb.Table('pageviewresume')
-
         response = table.update_item(
             Key={'id': 'counter'},
             UpdateExpression='SET #v = if_not_exists(#v, :start) + :inc',
-            ExpressionAttributeNames={
-                '#v': 'views'
-            },
-            ExpressionAttributeValues={
-                ':inc': 1,
-                ':start': 0
-            },
+            ExpressionAttributeNames={'#v': 'views'},
+            ExpressionAttributeValues={':inc': 1, ':start': 0},
             ReturnValues='UPDATED_NEW'
         )
-
         views = response['Attributes']['views']
         if isinstance(views, Decimal):
             views = int(views)
-
+        
         return {
             'statusCode': 200,
             'body': json.dumps({'views': views}),
-            'headers': {
-                'Content-Type': 'application/json'
-            }
+            'headers': {'Content-Type': 'application/json'}
         }
-
     except Exception as e:
-        print("ERROR:", str(e))
         return {
             'statusCode': 500,
             'body': json.dumps({'error': str(e)}),
-            'headers': {
-                'Content-Type': 'application/json'
-            }
+            'headers': {'Content-Type': 'application/json'}
         }
